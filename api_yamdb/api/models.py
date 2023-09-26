2b9c402ccad67611c29977ebb8da124742756067
Родион Шукало <ban.zhmyh@yandex.ru>
2023-09-26 17:05:43 +0300

    заготовка

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,1 +1,19 @@
-a = 1223
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Сategories(models.Model):
+
+
+class Genres(models.Model):
+
+
+class Reviews(models.Model):
+
+
+class Comments(models.Model):
+    
+
+class Titles(models.Model):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+
+from api.models import Сategories, Genres, Reviews, Comments, Titles
+from api.serializers import СategoriesSerializer, GenresSerializer, ReviewsSerializer, CommentsSerializer, TitlesSerializer
+
+
+class СategoriesViewSet(viewsets.ModelViewSet):
+
+    queryset = Сategories.objects.all()
+    serializer_class = СategoriesSerializer
+
+
+class GenresViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Genres.objects.all()
+    serializer_class = GenresSerializer
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+
+    queryset = Titles.objects.all()
+    serializer_class = TitlesSerializer
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
