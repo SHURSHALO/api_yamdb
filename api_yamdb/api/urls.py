c7fb7ce3ea35904d55ae322e7308b91c40d1bb20
Владислав Лутков <vlad786901@mail.ru>
2023-10-02 17:44:45 +0400

    url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TitleViewSet, GenreViewSet, CategoryViewSet, UsersViewSet
+from .views import TitleViewSet, GenreViewSet, CategoryViewSet, UsersViewSet, APIGetToken, APISignup
 
 router_v1 = DefaultRouter()
 router_v1.register(r'titles', TitleViewSet),
@@ -14,4 +14,6 @@ router_v1.register(
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
