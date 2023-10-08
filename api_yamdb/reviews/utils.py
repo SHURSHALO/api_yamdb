bc5cd33e9551e0ed3df80a9ae583a5f7cdfb0529
unknown <ban.zhmyh@yandex.ru>
2023-10-08 10:39:13 +0300

    utils fixes

--- api_yamdb/reviews/utils.py
+++ api_yamdb/reviews/utils.py
@@ -1,20 +1,16 @@
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
+MIN_SCORE = 1
+MAX_SCORE = 10
 
 def check_year_availability(year):
     current_year = timezone.now().year
     if year and year >= current_year:
         raise ValidationError(
-            'Год выпуска произведения не должен быть больше текущего.'
+            "Год выпуска произведения не должен быть больше текущего."
         )
 
-
 def check_score(score):
-    if not (1 <= score <= 10):
-        raise ValidationError('Оценка должна быть от 1 до 10')
-
-
-def check_score(score):
-    if not (1 <= score <= 10):
-        raise ValidationError('Оценка должна быть от 1 до 10')
+    if not (MIN_SCORE <= score <= MAX_SCORE):
+        raise ValidationError(f'Оценка должна быть от {MIN_SCORE} до {MAX_SCORE}')
